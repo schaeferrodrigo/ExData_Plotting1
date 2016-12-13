1acef1fda87923dadd08beb3b96148165456a9a4
Rodrigo Gonçalves Schaefer <schaeferrodrigo@gmail.com>
2016-12-13 23:22:49 +0100

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -2,7 +2,7 @@
 source("Download_data.R")
 
 
-data <- read.table("./data/household_power_consumption.txt", sep = ";", header = TRUE,data <- read.table("./data/household_power_consumption.txt", sep = ";", header = TRUE,na.strings = "Not Available", stringsAsFactors = FALSE))
+data <- read.table("./data/household_power_consumption.txt", sep = ";", header = TRUE,na.strings = "Not Available", stringsAsFactors = FALSE)
 data$Date <- as.Date(data$Date, "%d/%m/%Y")
 data <- data[data$Date == "2007-02-01"| data$Date == "2007-02-02",]
 data$Global_active_power <- as.numeric(data$Global_active_power)
